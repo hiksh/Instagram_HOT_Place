f1d52abd007dc3f2df2fe7e24b4075ed3fe016ec
Ddong <kt0230@kentech.ac.kr>
2023-10-29 16:21:19 +0900

    test.py upload

--- test.py
+++ test.py
@@ -1,17 +1,1 @@
-import subprocess
-import sys
-
-sys.stderr.reconfigure(encoding='utf-8')
-
-command = "git status"
-
-result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-print("-----------------------------------------------------")
-print("adding data to git")
-print("-----------------------------------------------------")
-
-print("result:\n", result.stdout)
-print("error:\n", result.stderr)
-
-print("finish")
+print('test')
