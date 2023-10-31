db20cf350f3133505b35dbfc40cdddb51e5a1525
Ddong <kt0230@kentech.ac.kr>
2023-10-31 14:50:22 +0900

    gpt upload

---
+++ findName.py
@@ -0,0 +1,28 @@
+import openai
+import pandas as pd
+import time
+openai.api_key = input("Enter the API_KEY: ")
+
+data = pd.read_csv('crawlingdata.csv', index_col=0)
+
+data['name'] = ""
+
+for idx, row in data.iterrows():
+    # print(row.loc["content"])
+    if row.loc["name"] != "":
+        continue
+    
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {"role":"system", "content":"you should find the name of restaurant"},
+            {"role":"user", "content": f'{row.loc["content"]} print only name of restaurant '},
+        ]
+    )
+
+    response= response.choices[0].message.content
+    print(f"response in {idx}: {response}")
+    row.loc["name"] = response
+    time.sleep(10)
+
+data.to_csv("crawlingdata.csv", mode='w')
